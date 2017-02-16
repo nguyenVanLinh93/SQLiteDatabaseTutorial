ea370702b6f6f8a5ab98b80a207088ac46d8340b
nguyenlinh <linh.nguyen93dhv@gmail.com>
2017-02-16 20:51:21 +0700

    set listview

--- app/src/main/java/com/example/nguyenlinh/sqlitedatabasetutorial/MainActivity.java
+++ app/src/main/java/com/example/nguyenlinh/sqlitedatabasetutorial/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.nguyenlinh.sqlitedatabasetutorial;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -33,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Reading all contacts
         Log.d("Reading: ", "Reading all contacts..");
-        List<Contact> contacts = db.getAllContacts();
+        final List<Contact> contacts = db.getAllContacts();
 
         /*for (Contact cn : contacts) {
             String log = "Id: "+cn.getID()+" ,Name: " + cn.getName() + " ,Phone: " + cn.getPhoneNumber();
@@ -46,5 +49,13 @@ public class MainActivity extends AppCompatActivity {
 
         // set adapter
         mListView.setAdapter(adapter);
+
+        // set onitemclick bat su kien khi click vao 1 item nao do
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), contacts.get(position).getID()+"vi tri"+position, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
